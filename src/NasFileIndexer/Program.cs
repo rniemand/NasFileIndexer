0992ed267d7ab0140d8c89a04de56727abde878a
Richard Niemand <niemand.richard@gmail.com>
2022-08-02 18:14:05 -0600

    Stubbing initial project

---
+++ src/NasFileIndexer/Program.cs
@@ -0,0 +1,10 @@
+using NasFileIndexer;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+      services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
