e06ead08fde276939d9f70a172db23e65e70884e
Richard Niemand <niemand.richard@gmail.com>
2022-08-04 07:42:59 -0600

    Prepping for new file format

--- src/NasFileIndexer.Common/Models/Entities/FileEntity.cs
+++ src/NasFileIndexer.Common/Models/Entities/FileEntity.cs
@@ -10,4 +10,19 @@ public class FileEntity
   public string Extension { get; set; } = string.Empty;
   public string DirectoryName { get; set; } = string.Empty;
   public string FileName { get; set; } = string.Empty;
+  public string PathSegment01 { get; set; } = string.Empty;
+  public string PathSegment02 { get; set; } = string.Empty;
+  public string PathSegment03 { get; set; } = string.Empty;
+  public string PathSegment04 { get; set; } = string.Empty;
+  public string PathSegment05 { get; set; } = string.Empty;
+  public string PathSegment06 { get; set; } = string.Empty;
+  public string PathSegment07 { get; set; } = string.Empty;
+  public string PathSegment08 { get; set; } = string.Empty;
+  public string PathSegment09 { get; set; } = string.Empty;
+  public string PathSegment10 { get; set; } = string.Empty;
+  public string PathSegment11 { get; set; } = string.Empty;
+  public string PathSegment12 { get; set; } = string.Empty;
+  public string PathSegment13 { get; set; } = string.Empty;
+  public string PathSegment14 { get; set; } = string.Empty;
+  public string PathSegment15 { get; set; } = string.Empty;
 }
