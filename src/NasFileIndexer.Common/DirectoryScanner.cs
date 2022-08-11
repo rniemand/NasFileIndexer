cc01549621597c9c90606453f081241ad748719e
Richard Niemand <niemand.richard@gmail.com>
2022-08-11 10:30:31 -0600

    WIP

--- src/NasFileIndexer.Common/DirectoryScanner.cs
+++ src/NasFileIndexer.Common/DirectoryScanner.cs
@@ -1,13 +1,18 @@
+using System.Text.RegularExpressions;
 using NasFileIndexer.Common.Mappers;
 using NasFileIndexer.Common.Models;
 using NasFileIndexer.Common.Repo;
+using Rn.NetCore.Common.Extensions;
+using Rn.NetCore.Common.Factories;
 using Rn.NetCore.Common.Logging;
+using Rn.NetCore.Common.Wrappers;
 
 namespace NasFileIndexer.Common;
 
 public interface IDirectoryScanner
 {
-  Task ScanAsync(NasFileIndexerConfig config, string path, CancellationToken stoppingToken);
+  IDirectoryScanner Configure(NasFileIndexerConfig config);
+  Task ScanAsync(string path, CancellationToken stoppingToken);
 }
 
 public class DirectoryScanner : IDirectoryScanner
@@ -15,21 +20,129 @@ public class DirectoryScanner : IDirectoryScanner
   private readonly ILoggerAdapter<DirectoryScanner> _logger;
   private readonly IFileRepo _fileRepo;
   private readonly IRepoObjectMapper _mapper;
+  private readonly IIOFactory _ioFactory;
+  private NasFileIndexerConfig _config = new();
 
   public DirectoryScanner(ILoggerAdapter<DirectoryScanner> logger,
     IFileRepo fileRepo,
-    IRepoObjectMapper mapper)
+    IRepoObjectMapper mapper,
+    IIOFactory ioFactory)
   {
     _logger = logger;
     _fileRepo = fileRepo;
     _mapper = mapper;
+    _ioFactory = ioFactory;
   }
 
-  public async Task ScanAsync(NasFileIndexerConfig config, string path, CancellationToken stoppingToken)
+  public IDirectoryScanner Configure(NasFileIndexerConfig config)
   {
+    _config = config;
+    return this;
+  }
 
+  public async Task ScanAsync(string path, CancellationToken stoppingToken)
+  {
+    if (stoppingToken.IsCancellationRequested)
+      return;
+
+    //if (!CanScanDirectory(path, depth, stoppingToken))
+    //  return;
+
+    //try
+    //{
+    //  _logger.LogInformation("Scanning directory depth {depth}: {path}", depth, path);
+    //  IDirectoryInfo directory = _ioFactory.GetDirectoryInfo(path);
+
+    //  foreach (IDirectoryInfo subDirInfo in directory.GetDirectories())
+    //    await ScanDirRecursive(subDirInfo.FullName, depth + 1, stoppingToken);
+
+    //  var files = new List<FileEntity>();
+    //  files.AddRange(directory.GetFiles().Select(_mapper.MapFileEntry));
+
+    //  await SaveResultsAsync(files, stoppingToken);
+    //}
+    //catch (Exception ex)
+    //{
+    //  _logger.LogError(ex, "Failed to index directory '{path}': {error}", path, ex.HumanStackTrace());
+    //}
 
 
     await Task.CompletedTask;
+  }
+
+  // Internal scanning methods
+  private async Task ScanDirRecursive(string path, int depth, CancellationToken stoppingToken)
+  {
+    if (!CanScanDirectory(path, depth, stoppingToken))
+      return;
+
+    try
+    {
+      _logger.LogInformation("Scanning directory depth {depth}: {path}", depth, path);
+      IDirectoryInfo directory = _ioFactory.GetDirectoryInfo(path);
+
+      foreach (IDirectoryInfo subDirInfo in directory.GetDirectories())
+        await ScanDirRecursive(subDirInfo.FullName, depth + 1, stoppingToken);
+
+      var files = new List<FileEntity>();
+      files.AddRange(directory.GetFiles().Select(_mapper.MapFileEntry));
+
+      await SaveResultsAsync(files, stoppingToken);
+    }
+    catch (Exception ex)
+    {
+      _logger.LogError(ex, "Failed to index directory '{path}': {error}", path, ex.HumanStackTrace());
+    }
+  }
+
+  private async Task SaveResultsAsync(List<FileEntity> files, CancellationToken stoppingToken)
+  {
+    if (stoppingToken.IsCancellationRequested)
+      return;
+
+    _logger.LogDebug("Saving {count} file(s) to the DB", files.Count);
+    await _fileRepo.AddManyAsync(files);
+  }
+
+  private bool CanScanDirectory(string path, int depth, CancellationToken stoppingToken)
+  {
+    // Check to see if a STOP was requested
+    if (stoppingToken.IsCancellationRequested)
+      return false;
+
+    // Check to see if we have hit the max folder depth
+    if (depth > _config.MaxScanDepth)
+      return false;
+
+    // Check to see if we have any configured skip paths
+    if (_config.SkipPaths.Length > 0)
+    {
+      if (_config.SkipPaths.Any(x => x.IgnoreCaseEquals(path)))
+      {
+        _logger.LogTrace("Skipping configured path: {path}", path);
+        return false;
+      }
+    }
+
+    // Run RegEx patterns to see if we need to exclude this file
+    if (_config.SkipPathExpressions.Length <= 0)
+      return false;
+
+    return !MatchesAnyExcludePattern(path);
+  }
+
+  private bool MatchesAnyExcludePattern(string path)
+  {
+
+    foreach (var expression in _config.SkipPathExpressions)
+    {
+      if (!Regex.IsMatch(path, expression, RegexOptions.IgnoreCase | RegexOptions.Singleline))
+        continue;
+
+      _logger.LogDebug("Skipping path based on RegEx pattern ({rxp}): {path}", expression, path);
+      return true;
+    }
+
+    return false;
   }
 }
--- src/NasFileIndexer.Common/Services/FileScannerService.cs
+++ src/NasFileIndexer.Common/Services/FileScannerService.cs
@@ -1,14 +1,9 @@
-using System.Text.RegularExpressions;
 using Microsoft.Extensions.DependencyInjection;
-using NasFileIndexer.Common.Mappers;
 using NasFileIndexer.Common.Models;
 using NasFileIndexer.Common.Providers;
 using NasFileIndexer.Common.Repo;
 using Rn.NetCore.Common.Abstractions;
-using Rn.NetCore.Common.Extensions;
-using Rn.NetCore.Common.Factories;
 using Rn.NetCore.Common.Logging;
-using Rn.NetCore.Common.Wrappers;
 
 namespace NasFileIndexer.Common.Services;
 
@@ -22,26 +17,20 @@ public class FileScannerService : IFileScannerService
   private readonly ILoggerAdapter<FileScannerService> _logger;
   private readonly IServiceProvider _serviceProvider;
   private readonly IDateTimeAbstraction _dateTime;
-  private readonly IRepoObjectMapper _mapper;
-  private readonly IIOFactory _ioFactory;
   private readonly IFileRepo _fileRepo;
   private readonly NasFileIndexerConfig _config;
   private DateTime _nextScanTime;
 
   public FileScannerService(ILoggerAdapter<FileScannerService> logger,
     IDateTimeAbstraction dateTime,
-    IIOFactory ioFactory,
     IConfigProvider configProvider,
     IFileRepo fileRepo,
-    IServiceProvider serviceProvider,
-    IRepoObjectMapper mapper)
+    IServiceProvider serviceProvider)
   {
     _logger = logger;
     _dateTime = dateTime;
-    _ioFactory = ioFactory;
     _fileRepo = fileRepo;
     _serviceProvider = serviceProvider;
-    _mapper = mapper;
 
     _config = configProvider.GetConfig();
     _nextScanTime = _dateTime.Now.AddSeconds(1);
@@ -63,7 +52,7 @@ public class FileScannerService : IFileScannerService
     await _fileRepo.TruncateTableAsync();
 
     Parallel.ForEach(_config.ScanPaths, async (path) =>
-      await ScanDirRecursive(path, 1, stoppingToken));
+      await ScanDirRecursive(path, stoppingToken));
 
     _logger.LogInformation("Indexing completed");
     _nextScanTime = _dateTime.Now.AddHours(24);
@@ -71,85 +60,11 @@ public class FileScannerService : IFileScannerService
 
 
   // Internal methods
-  private async Task ScanDirRecursive(string path, int depth, CancellationToken stoppingToken)
+  private async Task ScanDirRecursive(string path, CancellationToken stoppingToken)
   {
-    if (!CanScanDirectory(path, depth, stoppingToken))
-      return;
-
     await _serviceProvider
       .GetRequiredService<IDirectoryScanner>()
-      .ScanAsync(_config, path, stoppingToken);
-
-    Console.WriteLine();
-    Console.WriteLine();
-
-    try
-    {
-      _logger.LogInformation("Scanning directory depth {depth}: {path}", depth, path);
-      IDirectoryInfo directory = _ioFactory.GetDirectoryInfo(path);
-
-      foreach (IDirectoryInfo subDirInfo in directory.GetDirectories())
-        await ScanDirRecursive(subDirInfo.FullName, depth + 1, stoppingToken);
-
-      var files = new List<FileEntity>();
-      files.AddRange(directory.GetFiles().Select(_mapper.MapFileEntry));
-
-      await SaveResultsAsync(files, stoppingToken);
-    }
-    catch (Exception ex)
-    {
-      _logger.LogError(ex, "Failed to index directory '{path}': {error}", path, ex.HumanStackTrace());
-    }
-  }
-
-  private async Task SaveResultsAsync(List<FileEntity> files, CancellationToken stoppingToken)
-  {
-    if (stoppingToken.IsCancellationRequested)
-      return;
-
-    _logger.LogDebug("Saving {count} file(s) to the DB", files.Count);
-    await _fileRepo.AddManyAsync(files);
-  }
-  
-  private bool CanScanDirectory(string path, int depth, CancellationToken stoppingToken)
-  {
-    // Check to see if a STOP was requested
-    if (stoppingToken.IsCancellationRequested)
-      return false;
-
-    // Check to see if we have hit the max folder depth
-    if (depth > _config.MaxScanDepth)
-      return false;
-
-    // Check to see if we have any configured skip paths
-    if (_config.SkipPaths.Length > 0)
-    {
-      if (_config.SkipPaths.Any(x => x.IgnoreCaseEquals(path)))
-      {
-        _logger.LogTrace("Skipping configured path: {path}", path);
-        return false;
-      }
-    }
-
-    // Run RegEx patterns to see if we need to exclude this file
-    if (_config.SkipPathExpressions.Length <= 0)
-      return false;
-
-    return !MatchesAnyExcludePattern(path);
-  }
-
-  private bool MatchesAnyExcludePattern(string path)
-  {
-
-    foreach (var expression in _config.SkipPathExpressions)
-    {
-      if (!Regex.IsMatch(path, expression, RegexOptions.IgnoreCase | RegexOptions.Singleline))
-        continue;
-
-      _logger.LogDebug("Skipping path based on RegEx pattern ({rxp}): {path}", expression, path);
-      return true;
-    }
-
-    return false;
+      .Configure(_config)
+      .ScanAsync(path, stoppingToken);
   }
 }
