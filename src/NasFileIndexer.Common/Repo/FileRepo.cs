21a0bae927bde0564a74492ca374ae04fa8f21d8
Richard Niemand <niemand.richard@gmail.com>
2022-09-14 15:01:53 -0600

    Switching to connection factory

--- src/NasFileIndexer.Common/Repo/FileRepo.cs
+++ src/NasFileIndexer.Common/Repo/FileRepo.cs
@@ -1,6 +1,4 @@
-using System.Data;
 using Dapper;
-using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using NasFileIndexer.Common.Models;
 using NasFileIndexer.Common.Queries;
@@ -17,75 +15,29 @@ public interface IFileRepo
 public class FileRepo : IFileRepo
 {
   private readonly IFileRepoQueries _queries;
-  private readonly MySqlConnection _connection;
-  private readonly object _lockObject = new();
+  private readonly IConnectionFactory _connectionFactory;
 
-  public FileRepo(IFileRepoQueries queries, IConfiguration configuration)
+  public FileRepo(IFileRepoQueries queries, IConnectionFactory connectionFactory)
   {
-    var connectionString = configuration.GetSection("ConnectionStrings:NasFileIndexer").Value ?? string.Empty;
-    _connection = new MySqlConnection(connectionString);
     _queries = queries;
+    _connectionFactory = connectionFactory;
   }
 
   public int TruncateTable()
   {
-    EnsureConnected();
-
-    int result;
-    lock (_lockObject)
-      result = _connection.Execute(_queries.TruncateTable());
-
-    return result;
+    using MySqlConnection connection = _connectionFactory.GetConnection();
+    return connection.Execute(_queries.TruncateTable());
   }
 
   public int Add(FileEntity fileEntity)
   {
-    EnsureConnected();
-
-    var result = 0;
-    lock (_lockObject)
-      result = _connection.Execute(_queries.Add(), fileEntity);
-
-    return result;
+    using MySqlConnection connection = _connectionFactory.GetConnection();
+    return connection.Execute(_queries.Add(), fileEntity);
   }
 
   public int AddMany(List<FileEntity> entries)
   {
-    EnsureConnected();
-
-    var result = 0;
-    lock (_lockObject)
-      result = _connection.Execute(_queries.Add(), entries);
-
-    return result;
-  }
-
-  private void EnsureConnected()
-  {
-    lock (_lockObject)
-    {
-      switch (_connection.State)
-      {
-        case ConnectionState.Open:
-          return;
-        case ConnectionState.Closed:
-          _connection.Open();
-          break;
-      }
-
-      if (_connection.State != ConnectionState.Broken)
-        return;
-
-      try
-      {
-        _connection.Close();
-      }
-      catch (Exception)
-      {
-        // swallow
-      }
-
-      _connection.Open();
-    }
+    using MySqlConnection connection = _connectionFactory.GetConnection();
+    return connection.Execute(_queries.Add(), entries);
   }
 }
